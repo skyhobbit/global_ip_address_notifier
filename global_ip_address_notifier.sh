030837cff953d7f44352a6ea39fdf704bd6ea12b
Koshikawa Yasunori <y-koshikawa@skyhobbit.co.jp>
2024-06-24 22:56:31 +0900

    リファクタリング

--- global_ip_address_notifier.sh
+++ global_ip_address_notifier.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
- 
+
+# Define constant
+IP_FILE="/tmp/global_ip_address_notifier"
+
+# Get the current IP address
 ip_address=$(curl -s ifconfig.io)
-if ! [ -e /tmp/global_ip_address_notifier ]; then
-  touch /tmp/global_ip_address_notifier
-fi
-past_ip_address=$(cat /tmp/global_ip_address_notifier)
- 
+
+# Read the past IP address (return an empty string if the file does not exist)
+past_ip_address=$(cat ${IP_FILE} 2>/dev/null || echo "")
+
+# If the IP address has changed, write the new IP address to the file
 if [ "${ip_address}" != "${past_ip_address}" ]; then
-  echo ${ip_address} | tee /tmp/global_ip_address_notifier
+  echo ${ip_address} | tee ${IP_FILE}
 fi
